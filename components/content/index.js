34611c192d5fb1f574c3980c9a82986e1eda2db3
coinswitch-dev <42647992+coinswitch-dev@users.noreply.github.com>
2021-02-03 12:13:51 +0530

    update to body content

--- components/content/index.js
+++ components/content/index.js
@@ -4,8 +4,26 @@ export default function Content() {
     return (
         <div className="container mx-auto">
             <div className={styles.content}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tortor cras sit netus urna, malesuada vulputate dolor. Enim ipsum aliquam vel tempor sed leo neque aliquam sagittis. Amet nisi, felis, ornare sed elit. Egestas lorem praesent cursus interdum. Lectus sem at a rutrum. A dapibus aenean tincidunt purus mi ut. Tristique placerat egestas lacus feugiat tellus senectus faucibus. Vitae suspendisse lacus vulputate laoreet enim. Mauris a, tortor fermentum quam augue. At senectus mollis sem mauris egestas egestas ut.
-                Libero consectetur commodo est eu elit aliquet sem vulputate ipsum. A nunc pellentesque turpis pharetra metus dolor lectus ac scelerisque. Integer cras at posuere sit mauris. Quis ridiculus ullamcorper fermentum, consectetur massa. Blandit aliquam tempus sollicitudin augue arcu eu. Faucibus cursus et fermentum at. Commodo massa sit pharetra,
+              <strong>#IndiaWantsBitcoin Campaign Introduction & Why Participate?</strong>
+              <br><br>
+              As they say, 'All great changes are preceded by chaos'.
+              <br><br>
+              One such chaos, our young but thriving crypto community in India comprising of 10 million+ users, and 300+ crypto start-ups are undergoing because of the impending Crypto ban bill that is on the line to be presented in Loksabha soon.
+              <br><br>
+              The most important part of this crypto ecosystem in India is the users of crypto, i.e. YOU. 
+              <br><br>
+              We know that you have a burning opinion about this matter, we know you want to do something about this, we know you want to play your part and express your feelings. 
+              <br><br>
+              That's where <strong>#IndiaWantsBitcoin</strong> comes into the picture to help you raise your VOICE and reach out to your Member Of Parliament of your constituencies to call for positive and progressive regulation of crypto markets in India instead of a regressive one to create a new Digital India which is in line with Prime Minister's goal of Self-Reliant India (Atmanirbhar Bharat)
+              <br><br>
+              So go ahead & send a personal email to your MPs in a couple of clicks to change the future course of cryptocurrencies in India by using this platform which is:
+              <br>
+              Of The Crypto People In India
+              By The Crypto People In India
+              For The Crypto People In India
+              <br><br>
+              Let's Gooooooo!!
+              <br>
             </div>
         </div>
     )
