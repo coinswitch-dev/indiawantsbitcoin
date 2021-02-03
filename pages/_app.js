ab839d2c1abfe74d12ba0d30f56d86b283109a5a
Amit Singh <amit@coinswitch.co>
2021-02-03 18:50:18 +0530

    [EXPERIMENT]: Changing route manually

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router'
 import '../styles/globals.css'
 import 'swiper/swiper.scss';
 import 'swiper/components/navigation/navigation.scss';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const route = window.location.pathname
+    if (route !== '/') {
+      router.push(route)
+    }
+  }, [])
   return <Component {...pageProps} />
 }
 
