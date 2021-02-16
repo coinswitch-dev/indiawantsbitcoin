47dc6ce9c3cc7067d9dd107897e99a9dac959878
Rohit531185 <79140388+Rohit531185@users.noreply.github.com>
2021-02-16 19:46:37 +0530

    Update index.js

--- components/content/index.js
+++ components/content/index.js
@@ -6,21 +6,23 @@ export default function Content() {
             <div className={styles.content}>
               <strong>The Block and Crypto Committee</strong>
               <br /><br />
-              <strong>Letter to Community</strong>
+              <strong>Letter to the community from
+                    Members of the Blockchain and Crypto Assets Committee of India
+              </strong>
               <br /><br />
-              Hodlers,
+              Dear Hodlers from India,
               <br /><br />
-              As you may be aware, a new bill is being introduced in the Lok Sabha that contemplates a ban on cryptocurrencies in India. While the scope of the ban is unclear, it is going to adversely impact our nascent crypto community.
+              As you may be aware, a new bill is being introduced in the Lok Sabha that contemplates a ban on "private" cryptocurrencies in India. While the scope of the ban is unclear, it is certain that such a step will adversely impact our nascent crypto community.
               <br /><br />
               In a short while, we have grown into a community of over 10 million holders, 300+ startups generating tens of thousands of jobs and hundreds of millions of dollars in revenue and taxes.
               <br /><br />
-              Before such ban is passed in haste, it is critical that the most important voice is heard -- and that is yours. Please write to your local member of parliament and to Shri. Anurag Thakur, Minister of State for Finance and let him know that #IndiaWantsBitcoin. Shri. Anurag Thakure is a technologically progressive politician who is in touch with the young Indian aspirations and we are confident he will do the right thing for India.
+              Before such ban is passed in haste, it is critical that the most important voice is heard - and that is ours. To amplify our voices and to take them to our elected representatives, here's your chance to write a note to your local member of parliament and to Shri. Anurag Thakur, Minister of State for Finance to let them know that #IndiaWantsBitcoin. Shri. Anurag Thakur is a technologically progressive politician who is in touch with the young Indian aspirations and we are confident he will do the right thing for India.
               <br /><br />
-              Crypto is creating a democratic financial internet where everyone is an equal participant, and can access financial products that were previously available only to the elite. Over 8M Indians are already saving, earning and investing in this thriving global crypto economy. Banning it would be like banning the internet in the 1990s and will set India back by years.
+              Across the globe, crypto is creating a democratic financial internet where everyone is an equal participant, and can access financial products that were previously available only to the elite. Over 8M Indians are already saving, earning and investing in this thriving global crypto economy. Banning it would be like banning the internet in the 1990s and will set India back by years.
               <br /><br />
               As innovators, we remain committed to prudent regulation that protects investors and assists law enforcement in prohibiting any illicit use of the crypto infrastructure.
               <br /><br />
-              Together let's make our HODL heard,
+              Together let's make our HODL heard, Jai Hind!
               <br /><br />
               #IndiaWantsBitcoin
               <br /><br />
